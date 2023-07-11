acd16885c450d643df1520bd52a826c2a8141e0f
PavanSai <pavansaivvit@gmail.com>
2023-07-11 16:15:56 +0000

    have to add and update timer functionality

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -4,15 +4,34 @@ import './index.css'
 class DigitalTimer extends Component {
   constructor(props) {
     super(props)
-    this.state = {a: 'b'}
+    this.state = {isPaused: false, timerValue: 25}
   }
 
   componentDidMount() {
     console.log('component did mount called')
   }
 
+  toggleStartPauseText = () => {
+    this.setState(prevState => ({isPaused: !prevState.isPaused}))
+  }
+
+  inCreaseTimer = () => {
+    this.setState(prevState => ({timerValue: prevState.timerValue + 1}))
+  }
+
+  DecreaseTimer = () => {
+    this.setState(prevState => ({timerValue: prevState.timerValue - 1}))
+  }
+
   render() {
-    console.log('render called')
+    const {isPaused} = this.state
+    const startPauseText = isPaused ? 'Pause' : 'Start'
+    const playPauseIcon = isPaused
+      ? 'https://assets.ccbp.in/frontend/react-js/pause-icon-img.png'
+      : 'https://assets.ccbp.in/frontend/react-js/play-icon-img.png'
+    const altForPlayPauseIcon = isPaused ? 'play icon' : 'pause icon'
+    const timerStatus = isPaused ? 'Running' : 'Paused'
+    const {timerValue} = this.state
     return (
       <div className="app-container">
         <div className="digital-timer-container">
@@ -21,26 +40,30 @@ class DigitalTimer extends Component {
             <div className="timer-status-bg-image-container">
               <div className="timer-and-timer-status-container">
                 <h1 className="time">25:00</h1>
-                <p className="time-status">Remaining</p>
+                <p className="time-status">{timerStatus}</p>
               </div>
             </div>
             <div className="timer-controls-container">
               <div className="play-pause-reset-container">
                 <div className="start-pause-container">
-                  <button className="button" type="button">
+                  <button
+                    className="button"
+                    type="button"
+                    onClick={this.toggleStartPauseText}
+                  >
                     <img
-                      src="https://assets.ccbp.in/frontend/react-js/play-icon-img.png"
-                      alt="play"
+                      src={playPauseIcon}
+                      alt={altForPlayPauseIcon}
                       className="image"
                     />
                   </button>
-                  <p className="text">Start</p>
+                  <p className="text">{startPauseText}</p>
                 </div>
                 <div className="reset-container">
                   <button className="button" type="button">
                     <img
                       src="https://assets.ccbp.in/frontend/react-js/reset-icon-img.png"
-                      alt="play"
+                      alt="reset icon"
                       className="image"
                     />
                   </button>
@@ -50,11 +73,19 @@ class DigitalTimer extends Component {
               <div className="set-timer-limit-container">
                 <p className="text">Set Timer Limit</p>
                 <div className="timer-limit-control-container">
-                  <button type="button" className="button minus">
+                  <button
+                    onClick={this.DecreaseTimer}
+                    type="button"
+                    className="button minus"
+                  >
                     -
                   </button>
-                  <p className="number">25</p>
-                  <button type="button" className="button plus">
+                  <p className="number">{timerValue}</p>
+                  <button
+                    onClick={this.inCreaseTimer}
+                    type="button"
+                    className="button plus"
+                  >
                     +
                   </button>
                 </div>
